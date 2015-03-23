811e3bb6a95dd32d3f9ae45af25a7e15bd52d1c8
g23988 <g23988@gmail.com>
2015-03-23 11:20:46 +0800

    open log & use mysqli

---
+++ application/logs/log-2015-03-20.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-03-20 02:55:46 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-03-20 02:55:48 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-03-20 02:56:00 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-03-20 02:56:00 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-03-20 02:56:04 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2015-03-20 02:56:06 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead C:\wamp\www\pandan\system\database\drivers\mysql\mysql_driver.php 91
